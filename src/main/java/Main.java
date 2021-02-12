22e782e2f8a110b5b2e38b341434ac265c56ec76
Alan Yao <alanyao.training@gmail.com>
2021-02-12 17:10:04 -0600

    Loaded JSON.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+import com.google.gson.Gson;
+import student.Layout;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+
+
 public class Main {
-    public static void main(String[] args) {
+    private static Layout layout;
+
+    public static void main(String[] args) throws FileNotFoundException {
         // Wishing you good luck on your Adventure!
+        Gson gson = new Gson();
+        Reader reader = new FileReader("src/main/resources/siebel.json");
+        layout = gson.fromJson(reader, Layout.class);
+
     }
 }
