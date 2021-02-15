4dfee3c132d64fe90a1c7b5afe9e5932bc4f8d79
Alan Yao <alanyao.training@gmail.com>
2021-02-15 00:45:46 -0600

    Changed while loop to do while loop.

--- src/main/java/student/adventure/GameEngine.java
+++ src/main/java/student/adventure/GameEngine.java
@@ -1,13 +1,12 @@
 package student.adventure;
 
-//import com.sun.tools.jdeprscan.scan.Scan;
-
 import com.google.gson.Gson;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.Reader;
 import java.util.*;
+
 
 public class GameEngine {
     private static Layout layout;
@@ -19,13 +18,17 @@ public class GameEngine {
         Map<String, Location> locationMap = Layout.generateLocationMap(layout);
         Player player = new Player();
         player.setCurrentLocation(locationMap.get("Helgen"));
-        String[] arguments = getUserInput();
-        while (!arguments[0].equals("quit") && !arguments[0].equals("exit")) {
+        String command;
+        do {
+            String[] arguments = getUserInput();
+            command = arguments[0];
             if (player.getCurrentLocation().getName().equals("Windhelm")) {
+                // Ending location reached.
                 break;
             }
-            arguments = getUserInput();
+
         }
+        while (!command.equals("quit") && !command.equals("exit"));
     }
 
     public static void loadJson() throws FileNotFoundException {
@@ -36,23 +39,28 @@ public class GameEngine {
 
     public static String[] getUserInput() {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("> ");
         String input = scanner.nextLine();
         String[] arguments = tokenizeString(input);
         while (arguments[0].length() == 0) {
+            System.out.print("> ");
             input = scanner.nextLine();
             arguments = tokenizeString(input);
         }
         while (!userCommands.contains(arguments[0])) {
             String joinedArguments = String.join(" ", arguments);
-            System.out.println("You don't understand " + joinedArguments + "!");
+            System.out.println("You don't understand \"" + joinedArguments + "\"!");
+            System.out.print("> ");
             input = scanner.nextLine();
             arguments = tokenizeString(input);
         }
+
         return arguments;
     }
 
     public static String[] tokenizeString(String input) {
         input = input.toLowerCase();
+
         return input.split("\\s+");
     }
 }
