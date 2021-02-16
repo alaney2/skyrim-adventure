b29b1bc1c128a90c5d3178ff2c56e74b2420c625
Alan Yao <alanyao.training@gmail.com>
2021-02-16 16:16:57 -0600

    Added streams to check console output.

--- src/test/java/student/adventure/AdventureTest.java
+++ src/test/java/student/adventure/AdventureTest.java
@@ -1,26 +1,45 @@
 package student.adventure;
 
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
+
 
 import com.google.gson.Gson;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.Reader;
+import java.io.*;
 
 
 public class AdventureTest {
-    Gson gson;
-    Reader reader;
-    Layout layout;
+    // ByteArrayOutputStream and PrintStream are used to test console output.
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    private static Gson gson;
+    private static Reader reader;
+    private static Layout layout;
 
     @Before
     public void setUp() throws FileNotFoundException {
         gson = new Gson();
         reader = new FileReader("src/main/resources/skyrim.json");
         layout = gson.fromJson(reader, Layout.class);
+    }
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
     }
 
     @Test(expected = FileNotFoundException.class)
@@ -42,6 +61,11 @@ public class AdventureTest {
 
     @Test
     public void goWithoutArgument() {
+
+    }
+
+    @Test
+    public void goNullDirection() {
 
     }
 
