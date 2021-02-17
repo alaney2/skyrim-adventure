4ea0daf9570cb6bce7995cb2c39c3bbcc1aedc46
Alan Yao <alanyao.training@gmail.com>
2021-02-16 20:43:44 -0600

    Added sleep to intro.

--- src/main/java/student/adventure/GameEngine.java
+++ src/main/java/student/adventure/GameEngine.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.Reader;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 
 public class GameEngine {
@@ -17,7 +18,7 @@ public class GameEngine {
      * Method that starts running the game.
      * @throws FileNotFoundException If JSON doesn't exist.
      */
-    public static void runGame() throws FileNotFoundException {
+    public static void runGame() throws FileNotFoundException, InterruptedException {
         loadJson();
         printGameIntro();
 
@@ -53,14 +54,17 @@ public class GameEngine {
     /**
      * Prints the dialogue at the start of the game.
      */
-    public static void printGameIntro() {
+    public static void printGameIntro() throws InterruptedException {
         System.out.println("Ralof: Hey, you. You’re finally awake. You were trying to cross the border, right?\n" +
                 "Walked right into that Imperial ambush, same as us, and that thief over there.");
         System.out.println();
+        TimeUnit.SECONDS.sleep(3);
+
         System.out.println("Lokir: Damn you Stormcloaks. Skyrim was fine until you came along. Empire was nice and lazy.\n" +
                 "If they hadn't been looking for you, I could’ve stolen that horse and been half way to Hammerfell.\n" +
                 "You there. You and me — we should be here. It’s these Stormcloaks the Empire wants.");
         System.out.println();
+        TimeUnit.SECONDS.sleep(3);
     }
 
     /**
